0d56bbe9ab188c727060f16c75e727b546c0175e
hi1280 <hi12801280@gmail.com>
2020-03-02 22:26:52 +0900

    fix v2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const os = require('os');
 const app = express();
 
-app.get('/', (req, res) => res.send(`This request was processed by host: ${os.hostname()}`));
+app.get('/', (req, res) => res.send(`Now v2. This request was processed by host: ${os.hostname()}`));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
